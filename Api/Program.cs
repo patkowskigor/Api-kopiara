1af8d4bff1ada72c20437d8af4c2d4637b11bd7d
Igor <Igor.patkowski7@gmail.com>
2024-06-09 20:38:29 +0200

    auth wip

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,11 +1,24 @@
 using Api.Configurations;
+using Api.Model.Entities.Identity;
 using Api.Model.Entities.Note;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Dodaj serwisy do kontenera.
 var connectionString = builder.Configuration.GetConnectionString("kopiarka");
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(connectionString, sqlServerOptions =>
+    {
+        sqlServerOptions.EnableRetryOnFailure(
+            maxRetryCount: 5, // Maksymalna liczba ponowieñ
+            maxRetryDelay: TimeSpan.FromSeconds(30), // Maksymalne opóŸnienie miêdzy ponowieniami
+            errorNumbersToAdd: null); // Mo¿esz dodaæ specyficzne numery b³êdów SQL, które maj¹ byæ objête retry policy
+    }));
+
 builder.Services.AddDbContext<NoteContext>(options =>
 {
     options.UseSqlServer(connectionString, sqlServerOptions =>
@@ -16,6 +29,11 @@ builder.Services.AddDbContext<NoteContext>(options =>
             errorNumbersToAdd: null); // Mo¿esz dodaæ specyficzne numery b³êdów SQL, które maj¹ byæ objête retry policy
     });
 });
+builder.Services.AddAuthorization();
+
+builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+    .AddEntityFrameworkStores<AuthDbContext>();
+
 
 builder.Services.AddControllers();
 // Dowiedz siê wiêcej o konfigurowaniu Swagger/OpenAPI na https://aka.ms/aspnetcore/swashbuckle
@@ -30,6 +48,8 @@ options.AddPolicy("AllowAll", p => p.AllowAnyOrigin().AllowAnyHeader().AllowAnyM
 //builder.Services.AddAutoMapper(typeof(MapperConfig));
 
 var app = builder.Build();
+
+app.MapIdentityApi<IdentityUser>();
 
 // Konfiguracja potoku ¿¹dañ HTTP.
 if (app.Environment.IsDevelopment())
